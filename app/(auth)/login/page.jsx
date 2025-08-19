eaf3d7a11df2f82759912f341c80569f5b928824
Leonardo Serrate <leonardoserrate9@gmail.com>
2025-08-19 15:08:05 -0400

    develop funcionando

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -48,7 +48,7 @@ export default function Login() {
       localStorage.setItem("token", token);
       localStorage.setItem("registro", form.user.trim());
 
-      router.push("/dashboard");
+      router.push("/profile");
     } catch (error) {
       console.error(error);
       setErr("Credenciales incorrectas");
