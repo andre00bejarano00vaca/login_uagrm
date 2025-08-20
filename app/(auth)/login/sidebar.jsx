e73182c7dbd77c46c7463a4c0a38670c6ddf5b06
CodeWithElias <eliaspumaclaure48@gmail.com>
2025-08-20 15:38:33 -0400

    mejoras en el sidebar

--- app/(auth)/login/sidebar.jsx
+++ app/(auth)/login/sidebar.jsx
@@ -1,6 +1,5 @@
 'use client'
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
-
+import { Calendar, Home, Inbox, Search, Settings, User, ReceiptText } from "lucide-react"
 import {
   Sidebar,
   SidebarContent,
@@ -11,58 +10,132 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar"
+import { useRouter } from "next/navigation"
 
-// Menu items.
 const items = [
-  {
-    title: "Home",
-    url: "#",
-    icon: Home,
+  { 
+    title: "Datos Personales",
+    url: "/login",
+    icon: User
   },
-  {
-    title: "Inbox",
-    url: "#",
-    icon: Inbox,
+  { 
+    title: "Certificados",
+    url: "/login",
+    icon: ReceiptText
   },
-  {
-    title: "Calendar",
-    url: "#",
-    icon: Calendar,
+  { 
+    title: "Historico",
+    url: "/login",
+    icon: Home 
   },
-  {
-    title: "Search",
-    url: "#",
-    icon: Search,
+  { 
+    title: "Avance Academico", 
+    url: "#", 
+    icon: Inbox 
   },
-  {
-    title: "Settings",
-    url: "#",
-    icon: Settings,
+  { 
+    title: "Boleta de Incripcion", 
+    url: "#", 
+    icon: Calendar 
+  },
+  { 
+    title: "Casos Especiales",
+    url: "/login",
+    icon: Home 
+  },
+  { 
+    title: "Malla Curricular",
+    url: "/login",
+    icon: Home 
+  },
+  { 
+    title: "Notas Semestrales",
+    url: "/login",
+    icon: Home 
+  },
+  { 
+    title: "Consulta de Libros",
+    url: "/login",
+    icon: Home 
+  },
+  { 
+    title: "Pagos en Caja", 
+    url: "#", 
+    icon: Search 
+  },
+  { 
+    title: "Bloqueos",
+    url: "/login",
+    icon: Home 
+  },
+  { 
+    title: "Avance de Contenidos",
+    url: "/login",
+    icon: Home 
+  },
+  { 
+    title: "Evaluacion Academica",
+    url: "/login",
+    icon: Home 
+  },
+  { 
+    title: "Fecha de Analisis",
+    url: "/login",
+    icon: Home 
+  },
+  { 
+    title: "Form. Beca",
+    url: "/login",
+    icon: Home 
+  },
+  { 
+    title: "Control de Materias",
+    url: "/login",
+    icon: Home 
+  },
+  { 
+    title: "Cambiar Contraseña",
+    url: "/login",
+    icon: Home 
+  },
+  { 
+    title: "Settings", 
+    url: "#", 
+    icon: Settings 
   },
 ]
 
 export function AppSidebar() {
+
+  const router = useRouter()
+
   return (
-    <Sidebar>
-      <SidebarContent>
-        <SidebarGroup>
-          <SidebarGroupLabel>Application</SidebarGroupLabel>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {items.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <a href={item.url}>
-                      <item.icon />
-                      <span>{item.title}</span>
-                    </a>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
-      </SidebarContent>
-    </Sidebar>
+      <Sidebar>
+        <SidebarContent className="bg-blue-900 text-blue-100">
+          <SidebarGroup>
+            <SidebarGroupLabel className="text-blue-200 text-[18px] pb-8 pt-8">Consultas Esdudiantes</SidebarGroupLabel>
+            <SidebarGroupContent className="">
+              <SidebarMenu className="">
+                {items.map((item) => (
+                  <SidebarMenuItem key={item.title} className="pb-2">
+                    <SidebarMenuButton
+                      asChild
+                      className="bg-blue-200 text-blue-800 hover:bg-blue-300 transition-colors"
+                    >
+                      <button
+                        onClick={() => router.push(item.url)}
+                        className="flex items-center gap-2 p-2 rounded w-full text-left"
+                      >
+                        <item.icon className="text-blue-800" />
+                        <span>{item.title}</span>
+                      </button>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        </SidebarContent>
+      </Sidebar>
   )
 }
